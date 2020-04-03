46c689e8037ce8a2bb65be89a9b8d5615d67122c
h12345jack <h12345jack@163.com>
2020-04-03 22:14:38 +0800

    update

--- handle_files.py
+++ handle_files.py
@@ -28,17 +28,23 @@ args = parser.parse_args()
 def rename_files():
     dirname = 'videos'
     flist = [os.path.join(dirname, fname) for fname in os.listdir(dirname)]
-    rs_f = open(os.path.join(dirname, 'map.txt'), 'w')
+    rs_f = open(os.path.join(dirname, 'map.txt'), 'w', encoding='utf8')
     for ind, fpath in enumerate(flist):
         name = f"{ind}.mp4"
         fpath2 = os.path.join(dirname, name)
         print(fpath, fpath2, file=rs_f)
+        rs_dir = os.path.join(dirname, f'{ind}')
+        if not os.path.exists(rs_dir):
+            os.mkdir(rs_dir)
         os.rename(fpath, fpath2)
-        cmdline = f'ffmpeg -i {fpath2} -r 30 -f image2 {ind}/%5d.png'
+        cmdline = f'ffmpeg -i {fpath2} -r 30 -f image2 {rs_dir}/%5d.png'
         subprocess.run(cmdline, shell=True)
         print(cmdline)
     rs_f.close()
 
+def main():
+    rename_files()
+    
 if __name__ == "__main__":
     main()
 
