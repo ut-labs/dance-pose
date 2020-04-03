bdc7a2287daaa3869f34e229056cf3a2e93331eb
h12345jack <h12345jack@163.com>
2020-04-03 19:26:12 +0800

    add handle_files.py

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# dance-pose-2
+# dance-pose
+
+## prepare
+### download video from iqiyi
+
+```
+pip install you-get
+you-get https://www.iqiyi.com/v_19rx3k0t0g.html?list=19rqwueh0q
+```
+
+### make videos to pictures
+
+
+
---
+++ handle_files.py
@@ -0,0 +1,44 @@
+import os
+import re
+import sys
+import time
+import json
+import math
+import random
+import pickle
+import logging
+import argparse
+import subprocess
+
+from collections import defaultdict
+
+import scipy as sc
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dirpath', default=BASE_DIR, help='当前目录')
+args = parser.parse_args()
+
+
+def rename_files():
+    dirname = 'videos'
+    flist = [os.path.join(dirname, fname) for fname in os.listdir(dirname)]
+    rs_f = open(os.path.join(dirname, 'map.txt'), 'w')
+    for ind, fpath in enumerate(flist):
+        name = f"{ind}.mp4"
+        fpath2 = os.path.join(dirname, name)
+        print(fpath, fpath2, file=rs_f)
+        os.rename(fpath, fpath2)
+        cmdline = f'ffmpeg -i {fpath2} -r 30 -f image2 {ind}/%5d.png'
+        subprocess.run(cmdline, shell=True)
+        print(cmdline)
+    rs_f.close()
+
+if __name__ == "__main__":
+    main()
+
